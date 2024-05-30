8486566a087b9002139ed69602bc711e5cf90561
KevinNavarreteParra <navark4@unlv.nevada.edu>
2024-05-30 08:48:04 -0700

    new function to create chicago drafts

---
+++ R/draft_chicago.R
@@ -0,0 +1,17 @@
+#' @title Draft a new R Markdown document using the Chicago AD style
+#'
+#' @param name String value for the name of the new R Markdown document
+#' @param ... Additional arguments to pass to `rmarkdown::draft`
+#'
+#' @importFrom rmarkdown draft
+#'
+#' @return A new R Markdown document using the Chicago AD style
+#' @export
+#'
+#' @examples
+#' try(kevins.templates::draft_chicago("my_new_document"))
+draft_chicago <- function(name, ...) {
+
+  rmarkdown::draft(name, template = "chicago_ad", package = "kevins.templates", ...)
+
+}
