57685b9987c2a8a90b1e16a0c6555a61d5e4c432
KevinNavarreteParra <navark4@unlv.nevada.edu>
2024-05-30 08:49:47 -0700

    updated description

--- R/draft_chicago.R
+++ R/draft_chicago.R
@@ -1,6 +1,6 @@
 #' @title Draft a new R Markdown document using the Chicago AD style
 #'
-#' @param name String value for the name of the new R Markdown document
+#' @param name String value for the name of the new R Markdown document (i.e., "my_new_document.Rmd")
 #' @param ... Additional arguments to pass to `rmarkdown::draft`
 #'
 #' @importFrom rmarkdown draft
@@ -9,7 +9,7 @@
 #' @export
 #'
 #' @examples
-#' try(kevins.templates::draft_chicago("my_new_document"))
+#' try(kevins.templates::draft_chicago("my_new_document.Rmd"))
 draft_chicago <- function(name, ...) {
 
   rmarkdown::draft(name, template = "chicago_ad", package = "kevins.templates", ...)
