e3ddaa00bfb0bf038905ca037e6c2f91f43a1dcf
KevinNavarreteParra <navark4@unlv.nevada.edu>
2024-05-29 20:38:56 -0700

    wrote initial version of the chicagoad function

---
+++ R/chicagoad.R
@@ -0,0 +1,22 @@
+#' @title Generate a PDF Document with the Chicago AD Template
+#'
+#' @param ... Arguments passable to `rmarkdown::word_document`
+#'
+#' @return A PDF document with the Chicago AD template
+#' @export
+#'
+#' @importFrom rmarkdown pdf_document
+#'
+#' @details
+#' Thank you to Steve Miller's {stevetemplates} library for providing a baseline for this function.
+#'
+#'
+#' @examples
+#' try(rmarkdown::render("path/to/your.Rmd", kevins.templates::chicagoad()))
+chicagoad <- function(...){
+  template <- system.file("rmarkdown", "templates", "chicago_ad", "Resources", "template.docx",
+                          package = "kevins.templates")
+  rmarkdown::pdf_document(template = template,
+                          latex_engine = "xelatex",
+                          ...)
+}
