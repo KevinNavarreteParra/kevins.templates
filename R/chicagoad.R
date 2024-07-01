cc379d5d7054606926ef41787b1b0bab95195e37
KevinNavarreteParra <navark4@unlv.nevada.edu>
2024-07-01 10:48:52 -0700

    deprecated old chicago package

--- R/chicagoad.R
+++ R/chicagoad.R
@@ -6,14 +6,24 @@
 #' @export
 #'
 #' @importFrom rmarkdown pdf_document
+#' @importFrom lifecycle deprecate_stop badge
 #'
 #' @details
 #' Thank you to Steve Miller's `stevetemplates` library for providing a baseline for this function.
+#'
+#' @description
+#' #' `r lifecycle::badge("deprecated")`
+#'
+#' This function was deprecated because it was originally intended for use with Word, not PDF, which has much more limited functionality for templates. Please use `draft_chicago` instead.
 #'
 #'
 #' @examples
 #' try(rmarkdown::render("path/to/your.Rmd", kevins.templates::chicagoad()))
 chicagoad <- function(...){
+
+  lifecycle::deprecate_stop(when = "2024-07-01",
+                            what = "This function is now deprecated. Please use `draft_chicago` instead.")
+
   template <- system.file("rmarkdown", "templates", "chicago_ad", "Resources", "template.docx",
                           package = "kevins.templates")
   rmarkdown::pdf_document(template = template,
